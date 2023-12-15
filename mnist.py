7ae247537dc3c76259ffa4cdfa033b65e86a6914
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2023-12-15 20:45:28 +0100

    few changes

--- mnist.py
+++ mnist.py
@@ -28,10 +28,12 @@ Y_test = np.array([int(y) for y in Y_test])
 #=============================================================
 
 n_epoch = 3000 + 1
-lr_rate = 0.01
+lr_rate = 0.05
 NN = NeuralNetwork([50, 50], n_epoch, f_act='relu')
 result = NN.train(X_train, Y_train, alpha=lr_rate, verbose=True)
-
+A = NN.predict(X_train[:, :N-N//4 ])
+NN.confmat(Y_train[:N-N//4], A, plot=True, k=0)
+#plt.savefig("conf_mat_fit_train.pdf")
 L_t = result['train_Loss']
 L_v = result['valid_Loss']
 
@@ -43,9 +45,10 @@ plt.figure(1)
 plt.plot(np.linspace(1, n_epoch, n_epoch), L_t, 'b', label='train Loss')
 plt.plot(np.linspace(1, n_epoch, n_epoch), L_v, 'r', label='validation loss')
 plt.title('Binay cross entropy', fontsize=15)
-plt.xlabel('epoch')
-plt.ylabel('Loss')
+plt.xlabel('epoch', fontsize=15)
+plt.ylabel('Loss', fontsize=15)
 plt.legend(loc='best')
+#plt.savefig("Loss_fit.pdf")
 plt.grid()
 
 #=============================================================
@@ -53,9 +56,9 @@ plt.grid()
 #============================================================= 
 
 A = NN.predict(X_test)
-M = NN.confmat(Y_test, A, plot=True)
-
-acc = NN.accuracy(A, Y_test)
+M = NN.confmat(Y_test, A, plot=True, k=3)
+plt.savefig("conf_mat_fit_test.pdf")
+#acc = NN.accuracy(A, Y_test)
 print(f"Accuracy on test set = {acc:.5f}")
 
 plt.figure(2, figsize=(16, 10))
@@ -67,5 +70,6 @@ for i in range(40):
     plt.imshow(image)
 
 plt.tight_layout()
-plt.savefig("MNIST.pdf")
+
+#plt.savefig("MNIST.pdf")
 plt.show()
