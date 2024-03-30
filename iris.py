b1b22219b8dd8d4c20f180496c1bd30a2dab726f
Francesco Zeno Costanzo <zenofrancesco99@gmail.com>
2024-03-30 22:58:33 +0100

    few changes

--- iris.py
+++ iris.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
-from neural import NeuralNetwork
+from neural import NeuralNetworkClassifier
 
 #dati che verrano utilizzati
 iris_dataset = datasets.load_iris()
-print(iris_dataset["DESCR"])
+#print(iris_dataset["DESCR"])
 
 #caratteristiche, dati in input
 X = iris_dataset.data
@@ -18,16 +18,20 @@ Y = iris_dataset.target
 #testare se il modello ha imparato bene
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y)
 X_train, X_test = X_train.T, X_test.T
+
 #=============================================================
 # Parameter of computation and train of the network
 #=============================================================
-N = len(X_train[0,:])
+
+N       = len(X_train[0,:])
 n_epoch = 400 + 1
 lr_rate = 0.01
-NN = NeuralNetwork([50, 50], n_epoch, f_act='relu')
+
+NN     = NeuralNetworkClassifier([50, 50], n_epoch, f_act='relu')
 result = NN.train(X_train, Y_train, alpha=lr_rate, verbose=True)
-A = NN.predict(X_train[:, :N-N//4 ])
-NN.confmat(Y_train[:N-N//4], A, plot=True, k=0)
+A      = NN.predict(X_train[:, :N-N//4 ])
+
+NN.confmat(Y_train[:N-N//4], A, plot=True, title='Confusion matrix for train data', k=0)
 #plt.savefig("conf_mat_fit_train.pdf")
 L_t = result['train_Loss']
 L_v = result['valid_Loss']
@@ -51,11 +55,10 @@ plt.grid()
 #============================================================= 
 
 A = NN.predict(X_test)
-M = NN.confmat(Y_test, A, plot=True, k=2)
+M = NN.confmat(Y_test, A, plot=True, title='Confusion matrix for test data', k=2)
 #plt.savefig("conf_mat_fit_test.pdf")
 acc = NN.accuracy(A, Y_test)
 print(f"Accuracy on test set = {acc:.5f}")
 
 plt.tight_layout()
-#plt.savefig("MNIST.pdf")
 plt.show()
